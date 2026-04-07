d1b7e233436d68cfa7f2d86dbf557cf00d67b146
Zelenov Serhii <stalkercrying@gmail.com>
2026-04-07 20:47:16 +0200

    Add Selenium homework: open demowebshop test

---
+++ src/test/java/FirstSeleniumTest.java
@@ -0,0 +1,31 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FirstSeleniumTest {
+    WebDriver driver;
+    // before -> setUP
+    @BeforeMethod
+    public void setUP() {
+        WebDriverManager.chromedriver().browserVersion("146").setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com/"); // without history
+        // driver.navigate().to("https://www.google.com/"));  -> with history
+        // driver.navigate().back();
+        // driver.navigate().forward();
+        // driver.navigate().refresh();
+    }
+    // test
+    @Test
+    public void openGoogle() {
+        System.out.println("Hello");
+    }
+    // after -> tearDOWN
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
